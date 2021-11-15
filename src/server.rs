dea13333cb2d8908c457f9e230ed4384f5aeaaf0
Amanda Graven <amanda@amandag.net>
2021-11-15 11:40:14 +0100

    feat: Add logging with tracing

--- src/server.rs
+++ src/server.rs
@@ -4,6 +4,7 @@ use std::net::{IpAddr, SocketAddr};
 
 use serde::{Deserialize, Serialize};
 use trust_dns_resolver::{error::ResolveError, TokioAsyncResolver};
+use tracing::{debug, info, instrument};
 
 pub mod error;
 
@@ -85,48 +86,70 @@ impl Resolver {
 	}
 
 	/// Resolve the given server name
+	#[instrument(skip(self), err)]
 	pub async fn resolve(&self, name: &str) -> error::Result<Server> {
 		// 1. The host is an ip literal
+		debug!("Parsing socket literal");
 		if let Ok(addr) = name.parse::<SocketAddr>() {
+			info!("The server name is a socket literal");
 			return Ok(Server::Socket(addr));
 		}
+		debug!("Parsing IP literal");
 		if let Ok(addr) = name.parse::<IpAddr>() {
+			info!("The server name is an IP literal");
 			return Ok(Server::Ip(addr));
 		}
 		// 2. The host is not an ip literal, but includes a port
+		debug!("Parsing host with port");
 		if split_port(name).is_some() {
+			info!("The servername is a host with port");
 			return Ok(Server::HostPort(name.to_owned()));
 		}
 		// 3. Query the .well-known endpoint
+		debug!("Querying well known");
 		if let Some(well_known) = self.well_known(name).await? {
+			debug!("Well-known received: {:?}", &well_known);
 			// 3.1 delegated_hostname is an ip literal
+			debug!("Parsing delegated socket literal");
 			if let Ok(addr) = well_known.server.parse::<SocketAddr>() {
+				info!("The server name is a delegated IP literal");
 				return Ok(Server::Socket(addr));
 			}
+			debug!("Parsing delegated IP literal");
 			if let Ok(addr) = well_known.server.parse::<IpAddr>() {
+				info!("The server name is a delegated socket literal");
 				return Ok(Server::Ip(addr));
 			}
 			// 3.2 delegated_hostname includes a port
+			debug!("Parsing delegated hostname with port");
 			if split_port(&well_known.server).is_some() {
+				info!("The server name is a delegated hostname with port");
 				return Ok(Server::HostPort(well_known.server));
 			}
 			// 3.3 Look up SRV record
+			debug!("Looking up SRV record for delegated hostname");
 			if let Some(name) = self.srv_lookup(&well_known.server).await {
+				info!("The server name is a delegated SRV record");
 				return Ok(Server::Srv(name, well_known.server));
 			}
 			// 3.4 Use hostname in .well-known
+			debug!("Using delegated hostname directly");
 			return Ok(Server::Host(well_known.server));
 		}
 		// 4. The .well-known lookup failed, query SRV
+		debug!("Looking up SRV record for hostname");
 		if let Some(srv) = self.srv_lookup(name).await {
+			info!("The server name is an SRV record");
 			return Ok(Server::Srv(srv, name.to_owned()));
 		}
 		// 5. No SRV record found, use hostname
+		debug!("Using provided hostname directly");
 		Ok(Server::Host(name.to_owned()))
 	}
 
 	/// Query the .well-known information for a host.
 	#[cfg_attr(test, allow(unused_variables))]
+	#[instrument(skip(self, name), err)]
 	async fn well_known(&self, name: &str) -> error::Result<Option<ServerWellKnown>> {
 		#[cfg(not(test))]
 		let response = self.http.get(format!("https://{}/.well-known/matrix/server", name)).send().await;
@@ -149,6 +172,7 @@ impl Resolver {
 	}
 
 	/// Query the matrix SRV DNS record for a hostname
+	#[instrument(skip(self, name))]
 	async fn srv_lookup(&self, name: &str) -> Option<String> {
 		let srv = self.resolver.srv_lookup(format!("_matrix._tcp.{}", name)).await.ok()?;
 		// Get a record with the lowest priority value
