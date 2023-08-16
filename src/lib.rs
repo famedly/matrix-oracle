0903cc831e20fa93a95cd29bf5ae0a2f0baffd3d
kate-shine <github-action@actions-template-sync.noreply.github.com>
2023-08-16 07:21:23 +0000

    chore(template): merge template changes :up:

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,7 @@
-//! Matrix-oracle is a crate for performing lookups of .well-known information
-//! for the matrix protocol.
-//!
-//! # Features
-#![cfg_attr(doc, doc = document_features::document_features!())]
-#![deny(
-	trivial_casts,
-	trivial_numeric_casts,
-	unused_extern_crates,
-	unused_import_braces,
-	unused_qualifications
-)]
-#![warn(
-	missing_debug_implementations,
-	missing_docs,
-	dead_code,
-	clippy::unwrap_used,
-	clippy::expect_used
-)]
+//! TODO: crate documentation
 
-use http_cache_reqwest::{Cache, CacheMode, CacheOptions, HttpCache, MokaManager};
-
-#[cfg(feature = "client")]
-pub mod client;
-#[cfg(feature = "server")]
-pub mod server;
-
-/// Returns a HTTP caching middleware with appropriate settings for
-/// matrix-oracle's use-case.
-pub(crate) fn cache() -> Cache<MokaManager> {
-	Cache(HttpCache {
-		mode: CacheMode::Default,
-		manager: MokaManager::default(),
-		options: Some(CacheOptions { shared: false, ..CacheOptions::default() }),
-	})
+/// Write a hello world message
+#[allow(clippy::print_stdout)]
+pub fn hello_world() {
+	println!("Hello, world!");
 }
